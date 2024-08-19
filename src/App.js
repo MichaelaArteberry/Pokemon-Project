4a042a36e42d09bc48e9647c9842d0deaf2c2610
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-08-19 15:03:12 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import {useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+useEffect(() => {
+
+  const URL = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+  axios.get(URL).then((response) => {
+    console.log(response.data);
+  })
+
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon</h1>
     </div>
   );
 }
