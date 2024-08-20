eede14522b94641c5068cbbe57781b5c84ef61be
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-08-20 14:34:32 -0400

    added contants to contain api url

--- src/hooks/usePokemons.ts
+++ src/hooks/usePokemons.ts
@@ -4,6 +4,10 @@ import { IndexedPokemon } from '../interface/pokemon.interface';
 const usePokemons = () => {
     const [pokemons, setPokemons] = useState<IndexedPokemon[]>([]);
 
+    const fetchPokemon = async () => {
+
+    }
+
     return {
         pokemons,
     };
