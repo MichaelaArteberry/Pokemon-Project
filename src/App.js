93f3f0b20774b5dc6d97d61c1315d0fa08a7dd92
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-09-11 09:45:16 -0400

    Making sure its up to date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './pages/Home.tsx';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { theme } from './theme.ts';
 
 const router = createBrowserRouter([
   {
@@ -10,7 +13,12 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return(
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  )
 }
 
 export default App;
