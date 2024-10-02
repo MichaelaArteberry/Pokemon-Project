292a4b50485036eba69cf122a11f58e0ce4a044d
Michaela Arteberry <michaela.arteberry@gmail.com>
2024-10-02 12:17:34 -0400

    made some changes to the pokemon list component

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,21 +1,26 @@
 import React from "react";
 import { IndexedPokemon } from "../interface/pokemon.interface";
 import PokemonCard from "./PokemonCard.tsx";
+import { Grid } from "@mui/material";
 
 interface PokemonListProps {
-    pokemons: IndexedPokemon[];
+pokemons: IndexedPokemon[];
 }
 
 const PokemonList = ({ pokemons }: PokemonListProps) => {
-    return (
-        <>
-        { pokemons.length > 0
-            ? pokemons.map((p) => {
-                return <PokemonCard key = { p.name } pokemon = { p }/>
+return (
+    <Grid container spacing={2}>
+    {pokemons.length > 0
+        ? pokemons.map((p) => {
+            return (
+            <Grid item xs={4}>
+                <PokemonCard key={p.name} pokemon={p} />
+            </Grid>
+            );
         })
-        : null }
-        </>
-        )
+        : null}
+    </Grid>
+);
 };
 
 export default PokemonList;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import usePokemons from "../hooks/usePokemons.ts";
 import PokemonList from "../components/PokemonList.tsx";
+import { Container } from "@mui/material";
 
 const Home = () => {
 const { pokemons } = usePokemons();
 
-    return <PokemonList pokemons={pokemons} />;
+    return(
+        <Container>
+            <PokemonList pokemons={pokemons} />
+        </Container>
+    )
 
 };
 
